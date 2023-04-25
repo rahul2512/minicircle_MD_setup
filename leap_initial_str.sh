0751316ebc185dd9b84dada1e058b8a3ff23dae1
rahul2512 <rs25.iitr@gmail.com>
2023-04-25 21:19:41 +0200

    update

--- leap_initial_str.sh
+++ leap_initial_str.sh
@@ -9,14 +9,14 @@ INDEX='1'
 rm leap.log
 cp template.leap.1 leap.1.in
 
-sed -i.original 's/XX1/1/g' leap.1.in
-sed -i.original "s/YY1/${lseq}/g" leap.1.in
-sed -i.original "s/XX2/$[lseq+1]/g" leap.1.in
-sed -i.original "s/YY2/$[lseq+lseq]/g" leap.1.in
+sed -i 's/XX1/1/g' leap.1.in
+sed -i "s/YY1/${lseq}/g" leap.1.in
+sed -i "s/XX2/$[lseq+1]/g" leap.1.in
+sed -i "s/YY2/$[lseq+lseq]/g" leap.1.in
 
-tleap -f leap.1.in
+/work/scitas-share/ddossant/amber/20/gcc-8.4.0-cuda-10.2/amber20/bin/tleap -f leap.1.in
 
-rm tmp.pdb *.original
+rm tmp.pdb
 
 
 NWat=$(grep "O   WAT" solvated.pdb  | wc -l )
@@ -27,15 +27,17 @@ echo Number of water ${NWat} and at 0.15 molarity Nions  ${Nions}
 cp template.leap.2 leap.2.in
 
 
-sed -i.original "s/UUUU/${Nions}/g" leap.2.in
+sed -i "s/UUUU/${Nions}/g" leap.2.in
 
-tleap -f leap.2.in
-
-rm *.original
+/work/scitas-share/ddossant/amber/20/gcc-8.4.0-cuda-10.2/amber20/bin/tleap -f leap.2.in
 
 cp template.traj traj.1.in
 
-cpptraj traj.1.in
+/work/scitas-share/ddossant/amber/18/gcc-7.4.0/bin/cpptraj traj.1.in
+
+
+cp template.leap.3 leap.3.in
+/work/scitas-share/ddossant/amber/20/gcc-8.4.0-cuda-10.2/amber20/bin/tleap -f leap.3.in
 
 
 cp solvated.randions.crd ${RUNNAME}.crd 
